3fb9df5387b76fb2c37bf4b3c64d472d589578ca
tw0517tw <tw0517tw@gmail.com>
2017-08-12 00:14:58 +0800

    simplify

---
+++ README.md
@@ -0,0 +1,3 @@
+## travis badge
+
+[![Build Status](https://travis-ci.org/tw0517tw/rollbar-overflow.svg?branch=master)](https://travis-ci.org/tw0517tw/rollbar-overflow)
--- index.js
+++ index.js
@@ -29,7 +29,7 @@ async function createServer() {
           custom: {
             request: ctx.request,
           },
-        },
+        }
       );
       ctx.body = { error: error.message };
     }
